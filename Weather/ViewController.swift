1e49b7d0198975b1e3febd6302323e0281c0ec90
Ainor Syahrizal <macintorch@gmail.com>
2017-08-31 23:35:20 +0800

    url request and info plist setup

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var cityTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBAction func getWeatherButton(_ sender: Any) {
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "http://www.weather-forecast.com/locations/Semenyih/forecasts/latest")
+        
+        let request = NSMutableURLRequest(url: url!)
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            
+            data, response, error in
+            
+            if error != nil {
+                print(error)
+            } else {
+                if let unwrappedData = data {
+                    
+                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                    
+                    print(dataString)
+                }
+            }
+        }
+        
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {

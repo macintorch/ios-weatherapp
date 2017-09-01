17f06499045a8c371520e23c4ebde6f9c29edde5
Ainor Syahrizal <macintorch@gmail.com>
2017-09-02 04:51:50 +0800

    user enters city in textfield

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -16,20 +16,18 @@ class ViewController: UIViewController {
     
     
     @IBAction func getWeatherButton(_ sender: Any) {
-    }
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        let url = URL(string: "http://www.weather-forecast.com/locations/Semenyih/forecasts/latest")
+        if let url = URL(string: "http://www.weather-forecast.com/locations/" + cityTextField.text!.replacingOccurrences(of: " ", with: "-") + "/forecasts/latest") {
+            
         
-        let request = NSMutableURLRequest(url: url!)
+         let request = NSMutableURLRequest(url: url)
         
-        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+        
+         let task = URLSession.shared.dataTask(with: request as URLRequest) {
             
             data, response, error in
+            
+            var message = ""
             
             if error != nil {
                 print(error)
@@ -37,31 +35,52 @@ class ViewController: UIViewController {
                 if let unwrappedData = data {
                     
                     let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
-                 
+                    
                     var stringSeparator = "Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">"
                     
                     if let contentArray = dataString?.components(separatedBy: stringSeparator) {
                         
-                        if contentArray.count > 0 {
+                        if contentArray.count > 1 {
                             stringSeparator = "</span>"
                             
                             let newContentArray = contentArray[1].components(separatedBy: stringSeparator)
-                                if newContentArray.count > 0 {
-                                    
-                                    var message = newContentArray[0]
-                                    
-                                    print(newContentArray[0])
-                                }
+                            if newContentArray.count > 1 {
+                                
+                                message = newContentArray[0].replacingOccurrences(of: "&deg;", with: "°")
+                            
+                                print(message)
+                            }
                             
                         }
-                    
+                        
                     }
                 }
             }
-        }
+            if message == "" {
+                
+                message = "The weather there couldn't be found. Please try again."
+            }
+            
+            DispatchQueue.main.sync(execute: {
+                self.resultLabel.text = message
+            })
+            
+            
+         }
         
         task.resume()
+
+        } else {
+                resultLabel.text = "The Weather there coudn't be found. Please try again."
+        }
     }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+            }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
